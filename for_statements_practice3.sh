e4a64d11ed23f986107b3f74ad737e1ab18076e0
fa7271 <fa7271@naver.com>
2023-11-15 11:45:52 +0900

    linux_day2

---
+++ for_statements_practice3.sh
@@ -0,0 +1,25 @@
+echo "for statements practice3 start"
+if [ -d "mydir3" ]; then
+    cd mydir3
+else:
+    mkdir mydir3
+    cd mydir3
+# 파일 생성, 넣기
+
+for i in {1..100}
+    do
+    touch file$i.txt
+    for j in {1..100}
+    do
+        echo "hello wolrd$i" >> file$i.txt
+    done
+done
+for x in {1..100}
+    do
+    cat file$i.txt
+echo "practice complete"
+
+for i in {1..100}
+do
+    rm -rf file$i.txt
+done
